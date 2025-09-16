094ed181edc16d38c943e11cd23e7480369c18cb
CW-75 <cw75dev@gmail.com>
2025-09-15 23:49:49 -0400

    Added basic pattern example

---
+++ basics/patterns/basic.dart
@@ -0,0 +1,17 @@
+int number = 2;
+
+void main() {
+  switch (number) {
+    case 1:
+      print("Number is one");
+      break;
+    case 2:
+      print("Number is two");
+      break;
+    case 3:
+      print("Number is three");
+      break;
+    default:
+      print("Number is greater than three or less than one");
+  }
+}
